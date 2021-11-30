9de9e970045461760abd7cad15b63ca5bced7f5c
Ephil012 <Etp5501@gmail.com>
2021-11-30 13:43:45 -0500

    Changed path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='path/to/best.pt')  # or yolov5m, yolov5l, yolov5x, custom
 
 # Images
 img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
