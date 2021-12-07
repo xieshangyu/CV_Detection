a46bf76673d8ed24eb06b0adf9c2dac9e6f74cff
20af02 <ammaarfirozi@gmail.com>
2021-12-07 13:54:18 -0500

    Update uart.py

--- uart.py
+++ uart.py
@@ -1,1 +1,22 @@
-#Ammaar
+# Ammaar
+import time
+import serial
+
+count = 0
+
+ser = serial.Serial(
+    port="/dev/ttyTHS1",
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=1
+)
+
+
+def send_cords(horiz_disp, vert_disp):
+    global count
+    global ser
+    data = (str(horiz_disp) + ","+str(vert_disp))
+    ser.write(data.encode())
+    count += 1
