ed1cc408ea7415445bb815cd86e7bfc65a622e40
xieshangyu <2817476744@qq.com>
2022-02-24 19:21:14 -0500

    fixed bug pandas

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -11,9 +11,10 @@ def coordinate(frame, model):
     results = model(frame)                  #using the same model each frame
     multi_rows = results.pandas().xyxy[0]  
     row_1=multi_rows.head(1)                #this is the only way to return the first row of pandas coordinate
-    x_min, y_min, x_max,y_max = row_1
-    return x_min, y_min, x_max,y_max
-
+    if len(multi_rows.head(1)) != 0:
+        x_min, y_min, x_max,y_max = row_1['x_min'][0], row_1['y_min'][0],row_1['x_max'][0],row_1['y_max'][0]  
+        return x_min, y_min, x_max,y_max
+    return None
 
 
 
